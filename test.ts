2ccd67d8d83755cf36d1355941ee0f2b46790cc4
ibrahimhalilsezgin <ibrahimhalil0077sezgin@gmail.com>
2025-09-26 22:38:22 +0300

    delete test.ts

--- test.ts
+++
@@ -1,8 +0,0 @@
-
-function isValidEmail(email:string) {
-  const re = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-  return re.test(email);
-}
-
-
-console.log(isValidEmail("ibrahimhalil0077sezgin@gmail.com"))
